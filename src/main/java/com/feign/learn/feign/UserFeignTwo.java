e0849a5c4f8f23290d39d09f39242151f5f6bb24
侯凯旗 <alan.hou@pkfare.com>
2024-02-18 18:36:56 +0800

    feat: 增加Feign接口

--- src/main/java/com/feign/learn/feign/UserFeignTwo.java
+++ src/main/java/com/feign/learn/feign/UserFeignTwo.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "feign-learn")
-public interface UserFeign {
+@FeignClient(name = "feign-learn", url = "http://localhost:8010", contextId = "feign-learn-two")
+public interface UserFeignTwo {
 
   @GetMapping("/user/queryById")
   User queryById(@RequestParam Long id);
